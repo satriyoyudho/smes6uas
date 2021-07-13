a27b2df90130eac7d79cbcd42d01887d0b5e0200
satriyoyudho <rpl.satriyo@gmail.com>
2021-07-13 11:50:01 +0700

    p

---
+++ app/sysmenu.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class sysmenu extends Model
+{
+    protected $primaryKey = 'id';
+    protected $table = 'sysmenus';
+    public function categories()    {
+        return $this->hasMany(sysmenu::class);
+    }
+
+    public function childrenCategories()    {
+        return $this->hasMany(sysmenu::class)->with('categories');
+    }
+}
